04d6f63d181f49808c55fa05fb0e1c756ee3d1a6
AniKanayo <micanian4real@gmail.com>
2023-07-01 18:56:22 +0000

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    triangle = []
+
+    for i in range(n):
+        row = [1] * (i + 1)  # Create a row with i+1 items & initialized to 1
+
+        if i >= 2:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    return triangle
