db2ef3e9bb0181da6638b3610eb3a4c3d5415302
AniKanayo <micanian4real@gmail.com>
2023-08-24 20:13:09 +0000

    0x08-making_change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+"""
+This function calculates the fewest number of coins
+needed to meet a given total amount
+"""
+
 
 def makeChange(coins, total):
-    """ The docstring comes in here """
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed
+    to meet a given amount total
+    """
     if total <= 0:
         return 0
-    
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    
+    dp = [0] + [float("inf")] * (total)
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
-    
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[-1] if dp[-1] != float("inf") else -1
