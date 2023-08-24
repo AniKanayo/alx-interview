e8560f0ed836e52b263b59262d25c0265fef1677
AniKanayo <micanian4real@gmail.com>
2023-08-24 20:38:01 +0000

    0x08-making_change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -23,8 +23,8 @@ def makeChange(coins, total):
 
     dp = [0] + [float("inf")] * total
 
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+    for koin in coins:
+        for x in range(koin, total + 1):
+            dp[x] = min(dp[x], dp[x - koin] + 1)
 
     return dp[-1] if dp[-1] != float("inf") else -1
