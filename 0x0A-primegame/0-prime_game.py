e89af14ca196924c725a3583c61f00ba6b5d0091
AniKanayo <micanian4real@gmail.com>
2023-09-06 21:12:41 +0000

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -44,8 +44,8 @@ def findPrimes(n):
 
 def isWinner(x, nums):
     """
-    Prime Game: Maria and Ben are playing a game.Given a set of consecutive integers
-    starting from 1 up to and including n, they take turns choosing a
+    Prime Game: Maria and Ben are playing a game.Given a set of consecutive
+    integers starting from 1 up to and including n, they take turns choosing a
     prime number from the set and removing that number and its
     multiples from the set...
     """
